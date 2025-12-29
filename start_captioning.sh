775bc3faa4fc8e2c9c84aff87e22cf98180a2343
Katrin Tomanek <katrin.tomanek@gmx.de>
2025-12-28 20:02:42 -0800

    fix start script

--- start_captioning.sh
+++ start_captioning.sh
@@ -1,5 +1,5 @@
 source venv/bin/activate
 clear
-python src/captioning_app.py  --rich_captions --audio_input_device_index=1 --model moonshine_onnx_tiny
+python src/captioning_lib/captioning_app.py  --rich_captions --audio_input_device_index=1 --model moonshine_onnx_tiny
 
 # python src/captioning_app.py --show_audio_devices
